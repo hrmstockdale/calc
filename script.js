fd646bc04d46852da14fecbad953abc7c66de734
Henry Stockdale <hrmstockdale@gmail.com>
2019-03-03 19:24:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+let display = document.getElementById("screen");
+let buttons = document.getElementById("calculator-buttons");
+
+for (var i = 0; i < buttons.length; i++){
+    let button = button[i];
+
+    console.log(button);
+}
 
 
